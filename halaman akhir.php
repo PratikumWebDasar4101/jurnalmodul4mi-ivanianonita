4a5027def6018de1ba4a3305ad85e0a138e7f6ff
ivanianonita <42725307+ivanianonita@users.noreply.github.com>
2018-09-24 10:45:08 +0700

    Update halaman akhir.php

--- halaman akhir.php
+++ halaman akhir.php
@@ -1,1 +1,15 @@
-halaman akhir bisa diperlukan
+<?php
+
+$input = count($_POST['makanan']);
+for($i=0; $i<$input; $i++){
+	echo$_POST['makanan'][$i]."<br>";
+}
+$file= $_FILES['dokumen'];
+$nama_file= $file['name'];
+$nama_tmp = $file['tmp_name'];
+$upload_dir ="upload/";
+move_uploaded_file($nama_tmp,$upload_dir.$nama_file);
+echo"File berhasil diupload."
+?>
+<img src="upload/<?php echo $nama_file; ?>">
+<a href="checkbox.html"><input type="submit" name="delete" value="delete"></a>
